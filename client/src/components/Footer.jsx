f9351685baa314be2968aae615256d480bf46977
AchieverSana <sanaparveenjaipur786@gmail.com>
2026-05-25 14:32:21 +0530

    Refactor Footer component to use updated Flowbite syntax

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,4 @@
-import {
-  Footer,
-  FooterTitle,
-  FooterLinkGroup,
-  FooterLink,
-  FooterDivider,
-  FooterCopyright,
-  FooterIcon,
-} from 'flowbite-react';
+import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { SiLeetcode } from 'react-icons/si';
@@ -17,10 +9,7 @@ export default function FooterCom() {
       <div className='w-full max-w-7xl mx-auto'>
         <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
           <div className='mt-5'>
-            <Link
-              to='/'
-              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
-            >
+            <Link to='/' className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
               <span className='px-2 py-1 bg-gradient-to-r from-teal-400 via-cyan-500 to-blue-500 rounded-lg text-white'>
                 Sana's
               </span>
@@ -32,42 +21,36 @@ export default function FooterCom() {
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
             <div>
-              <FooterTitle title='About' />
-              <FooterLinkGroup col>
-                <FooterLink href='/about'>About Me</FooterLink>
-                <FooterLink href='/projects'>Projects</FooterLink>
-              </FooterLinkGroup>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='/about'>About Me</Footer.Link>
+                <Footer.Link href='/projects'>Projects</Footer.Link>
+              </Footer.LinkGroup>
             </div>
             <div>
-              <FooterTitle title='Find Me' />
-              <FooterLinkGroup col>
-                <FooterLink href='https://github.com/AcheiverSana' target='_blank' rel='noopener noreferrer'>
-                  GitHub
-                </FooterLink>
-                <FooterLink href='https://www.linkedin.com/in/sana-parveen-499316325/' target='_blank' rel='noopener noreferrer'>
-                  LinkedIn
-                </FooterLink>
-                <FooterLink href='https://leetcode.com/u/Achieversana/' target='_blank' rel='noopener noreferrer'>
-                  LeetCode
-                </FooterLink>
-              </FooterLinkGroup>
+              <Footer.Title title='Find Me' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='https://github.com/AcheiverSana' target='_blank'>GitHub</Footer.Link>
+                <Footer.Link href='https://www.linkedin.com/in/sana-parveen-499316325/' target='_blank'>LinkedIn</Footer.Link>
+                <Footer.Link href='https://leetcode.com/u/Achieversana/' target='_blank'>LeetCode</Footer.Link>
+              </Footer.LinkGroup>
             </div>
             <div>
-              <FooterTitle title='Legal' />
-              <FooterLinkGroup col>
-                <FooterLink href='#'>Privacy Policy</FooterLink>
-                <FooterLink href='#'>Terms &amp; Conditions</FooterLink>
-              </FooterLinkGroup>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
             </div>
           </div>
         </div>
-        <FooterDivider />
+        <Footer.Divider />
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
-          <FooterCopyright href='#' by="Sana's Blog" year={new Date().getFullYear()} />
+          <Footer.Copyright href='#' by="Sana's Blog" year={new Date().getFullYear()} />
           <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
-            <FooterIcon href='https://github.com/AcheiverSana' target='_blank' icon={BsGithub} />
-            <FooterIcon href='https://www.linkedin.com/in/sana-parveen-499316325/' target='_blank' icon={BsLinkedin} />
-            <FooterIcon href='https://leetcode.com/u/Achieversana/' target='_blank' icon={SiLeetcode} />
+            <Footer.Icon href='https://github.com/AcheiverSana' icon={BsGithub} />
+            <Footer.Icon href='https://www.linkedin.com/in/sana-parveen-499316325/' icon={BsLinkedin} />
+            <Footer.Icon href='https://leetcode.com/u/Achieversana/' icon={SiLeetcode} />
           </div>
         </div>
       </div>
