df6e6a1799330ddc17128ad498b190e63d7fefde
AchieverSana <sanaparveenjaipur786@gmail.com>
2026-05-25 11:46:40 +0530

    Refactor server start logic for local environment

--- api/index.js
+++ api/index.js
@@ -127,10 +127,8 @@ app.use((err, req, res, next) => {
 export default app;
 
 
-// Start server when run locally
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5001;
-  app.listen(PORT, () =>
-    console.log(`🚀 Server running on http://localhost:${PORT}`)
-  );
-}
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
