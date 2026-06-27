59287dfc72e5144818a138a4df69802179c949ab
AchieverSana <sanaparveenjaipur786@gmail.com>
2026-06-27 21:33:10 +0530

    Implement PrivateRoute for user authentication

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,1 +1,7 @@
-Redirecting...
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
+
+export default function PrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
+}
