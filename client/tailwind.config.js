6b225914e775759d7c3fc2c42f2e3536535e5942
AchieverSana <sanaparveenjaipur786@gmail.com>
2026-06-27 21:44:28 +0530

    Update Tailwind CSS configuration with plugins

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,1 +1,16 @@
-Redirecting...
+import tailwindScrollbar from 'tailwind-scrollbar';
+import flowbitePlugin from 'flowbite-react/plugin/tailwindcss';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/dist/**/*.{js,mjs}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindScrollbar, flowbitePlugin],
+};
