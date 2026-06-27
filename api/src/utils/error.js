6ea3acce30b83b34bade7d80d64d5c9b54d4ce97
AchieverSana <sanaparveenjaipur786@gmail.com>
2026-06-27 21:01:19 +0530

    Add errorHandler function for error handling

--- api/src/utils/error.js
+++ api/src/utils/error.js
@@ -1,1 +1,6 @@
-Redirecting...
+export const errorHandler = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};
