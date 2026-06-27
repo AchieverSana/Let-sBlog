30b24468719823e58802305ad7d53eccc97429b9
AchieverSana <sanaparveenjaipur786@gmail.com>
2026-06-27 21:45:00 +0530

    Initialize Vite config with server and plugins
    Added Vite configuration for server proxy and testing.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,1 +1,20 @@
-Redirecting...
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    globals: true,
+  },
+});
