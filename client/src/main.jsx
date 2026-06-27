3b206a31ffade389dffb52ed05ec9912a5b95977
AchieverSana <sanaparveenjaipur786@gmail.com>
2026-06-27 21:10:08 +0530

    Set up React app with Redux and error boundary

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,20 @@
-Redirecting...
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.jsx';
+import ErrorBoundary from './components/ErrorBoundary.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ErrorBoundary>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
+  </ErrorBoundary>
+);
